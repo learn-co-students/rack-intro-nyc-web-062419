1fce53a604725f1a44a9a761a0588665dae2a391
Ben Braunstein <benzbraunstein@gmail.com>
2019-07-14 01:33:13 -0400

    Done.

--- application.rb
+++ application.rb
@@ -1,10 +1,8 @@
 class Application
-
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is"
+    puts env
     resp.finish
   end
-
 end
-
